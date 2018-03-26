24a3a86eb5481c2041a3f0174bdc307762bfc5a5
Jonathan Günz <harmoniemand@googlemail.com>
2018-03-26 14:15:35 +0200

    added parameters to script

--- src/find-shares.ps1
+++ src/find-shares.ps1
@@ -1,14 +1,42 @@
-$machinename = "machinename"
 
-$users = @("user1")
+$hostname = "localhost";
+$user = $(Get-WMIObject -class Win32_ComputerSystem | select username).username
+$outfile = "console";
+$result = @();
 
-$users | ForEach-Object {
-    $user = $_
-    $shares = Get-WmiObject -class Win32_Share -computer $machinename
+for ($i = 0; $i -lt $args.Length; $i++) {
+    
+    if ($args[$i] -eq "--hostname") {
+        $hostname = $args[$i + 1];
+        $i++;
+    }
 
-    $shares | ForEach-Object {
-        $share = $_
+    if ($args[$i] -eq "--user") {
+        $user = $args[$i + 1];
+        $i++;
+    }
 
-        $( $user + "; " + $share.Path + "; ") | Out-File -FilePath 'out.csv' -Append
+    if ($args[$i] -eq "--out") {
+        $outfile = $args[$i + 1];
+        $i++;
     }
 }
+
+$shares = Get-WmiObject -class Win32_Share -computer $hostname
+
+foreach ($share in $shares) {
+    $obj = New-Object psobject;
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name User -Value $user;
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name Path -Value $share.Path;
+
+    $result += $obj;
+}
+
+if ($outfile -eq "console") {
+    Write-Output $result;
+} else {
+    foreach($r in $result) {
+        $( $r.User + "; " + $r.Path + "; ") | Out-File -FilePath 'out.csv' -Append
+    }
+}
+
